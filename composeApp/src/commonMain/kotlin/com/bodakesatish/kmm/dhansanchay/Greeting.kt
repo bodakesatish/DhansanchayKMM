ecc63fc4d924307f5f9e4a52dae33d3fd31450c0
Satish Bodake <satishbodake@Satishs-Mac-mini.local>
2025-07-06 19:56:59 +0530

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/com/bodakesatish/kmm/dhansanchay/Greeting.kt
@@ -0,0 +1,9 @@
+package com.bodakesatish.kmm.dhansanchay
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/bodakesatish/kmm/dhansanchay/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.bodakesatish.kmm.dhansanchay
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
